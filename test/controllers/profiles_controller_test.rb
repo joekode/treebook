392e8aba20c97836cb2186185af53f97590a51c0
Kode Creative <interact@wuyiuchung.com>
2014-01-23 09:20:02 +0000

    Moved sqllite to dev, test environ

---
+++ test/controllers/profiles_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class ProfilesControllerTest < ActionController::TestCase
+  test "should get show" do
+    get :show, id: users(:joe).profile_name
+    assert_response :success
+    assert_template 'profiles/show'
+  end
+
+  test "should render a 404 on profile not found" do
+  	get :show, id: "doesnt exist"
+  	assert_response :not_found
+  end
+
+  test "that variables are assigned on success profile viewing" do
+  	get :show, id: users(:joe).profile_name
+  	assert assigns(:user)
+  	assert_not_empty assigns(:statuses)
+  end
+
+   test "only shows the correct user's statuses" do
+  	get :show, id: users(:joe).profile_name
+  	assigns(:statuses).each do |status|
+  		assert_equal users(:joe), status.user
+  	end
+  end
+
+end
